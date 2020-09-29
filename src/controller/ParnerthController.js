7c58c78953ec73e781dae16933628ca81a126c30
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-09-28 20:21:27 -0500

    [MODIFY] Refactorisando Querys para las peticiones en las rutas Parnerth

--- src/controller/ParnerthController.js
+++ src/controller/ParnerthController.js
@@ -5,81 +5,74 @@ import shortid from  'shortid';
 
 class ParnerthController {
 
-  // TODO · Crear Enpoint para agregar parnerth solamente a un grupo 09/27/2020 
-  // TODO · Crear Enpoint para eliminar un parnerth globalmente  09/27/2020 
+
+  // ✓ DONE · Agregar un Parnerth global [Usuario + Grupo] & [Grupo] 09/27/2020 
+
+  // ✓ DONE · Mostrar grupos con Parnerth 09/27/2020 
+  // ✓ DONE · Mostrar un grupo con Parnerth 09/27/2020 
+
+  // TODO · Eliminar un Parnerth globalmente 09/27/2020 
+  // TODO · Eliminar un Parnerth de un Grupo 09/27/2020 
 
   async store(req, res) {
 
-    const { parnerth_key, group } = req.body;
+    const { key, group } = req.body;
+    
     let keyIsAdd = {};
-
-    if(!shortid.isValid(parnerth_key)) keyIsAdd = { ok: false, error: 400, message: 'EL valor enviado no es valido.' };
     
-    const parnerthUser = await User.findOne({parnerth_key: parnerth_key}, ['_id']);
-
-    if(!parnerthUser) keyIsAdd = { ok: false, error: 404, message: 'El KEY enviado no fue encontrado.' };
-    
-    if(parnerthUser) await User.findById(req.user_id,['parnerth'], (err, resp) => {
-      const { parnerth } = resp;
-
-      if(parnerth.includes(parnerthUser._id)){
-        keyIsAdd = { ok: false, error: 400, message: 'El KEY enviado ya esta agregado a tu lista.' };
-      };
-    });
-
     try {
+      
+      // Obtener ID del parnerth
+      const parnerthUser = await User.findOne({ parnerth_key: key }, ['_id']);
+      if(!parnerthUser) keyIsAdd= { ok: false, error: 404, message: 'El KEY no devuelve resultados.' };
+      
+      // Obtener el Grupo al que se le agregara el parnerth
+      const groupFound = await Group.findOneAndUpdate({ _id: group }, { parnerth: [parnerthUser._id] }, { new: true, select: 'name parnerth' })
+        .populate({ path: 'parnerth', select: 'name img -_id' });
+      if(!groupFound) keyIsAdd= { ok: false, error: 404, message: 'El GRUPO no devuelve resultados.' };
 
-      if(Object.keys(keyIsAdd).length === 0){
+      // Actualizar Usuario con el nuevo Parnerth
+      const userOnTurn = await User.findOneAndUpdate({ _id: req.user_id }, { parnerth: [parnerthUser._id] }, { new: true, select: 'name parnerth' })
+        .populate({ path: 'parnerth', select: 'name img -_id' });
 
-        User.updateOne({ _id: req.user_id }, { 
-          // $push: { parnerth: [parnerth_key] } 
-          parnerth: [parnerthUser._id]
-        }, (err, res) => {
-          if(err) console.log(err);
-          console.log(res);
-        });
-  
-        Group.updateOne({ _id: group }, { 
-          // $push: { parnerth: [parnerth_key] } 
-          parnerth: [parnerthUser._id]
-        }, (err, res) => {
-          if(err) console.log(err);
-          console.log(res);
-        });
+      const dataUpdate = {
+        group: groupFound,
+        user: userOnTurn
       }
 
       if(Object.keys(keyIsAdd).length > 0){
         return res.status(400).json(keyIsAdd);
       }else{
-        return res.json({ok: true, message: "Se elemento fue agregado correctamente."});
+        return res.json({ok: true, message: "Se elemento fue agregado correctamente.", dataUpdate});
       }
 
     } catch (error) {
       return res.status(400).json(error);
     }
 
-  }
+  };
 
   async index(req, res) {
 
     try {
       
-      const user = await User.findById(req.user_id, ['parnerth_key']);
+      // Obtener el usuario en turno para validar si es USUARIO o PARNERTH
+      const userOnTurn = await User.findById(req.user_id, ['parnerth_key']);
 
       let groups = null;
 
-      if(user.parnerth_key === null){
-        const gps = await Group.find({user_id: req.user_id}, ['name', 'color', 'parnerth'])
+      // Validar si el Usuario en turno tiene un ['parnerth_key] o no
+      // Si es NUll es Usuario
+      // Si es EXISTE es Parnerth
+      if(userOnTurn.parnerth_key === null){
+
+        groups = await Group.find({user_id: req.user_id, parnerth: {$exists: true, $not: {$size: 0}}}, ['name', 'color', 'parnerth', 'createdAt'])
           .populate({ path: 'parnerth', select: 'name img -_id' })
           .sort({ 'createdAt': -1 });
 
-        const newGps = gps.filter(a => a.parnerth.length > 0);
-
-        groups = newGps;
-
       }else{
         groups = await Group.find({parnerth: {$all:[req.user_id]}}, ['name', 'color', 'user_id'])
-          .populate({ path: 'user_id', select: 'name img' })
+          .populate({ path: 'user_id', select: 'name img -_id' })
           .sort({ 'createdAt': -1 });
       };
 
@@ -96,34 +89,44 @@ class ParnerthController {
 
     if(param.length !== 24) res.status(400).json({ ok: false, error: 400, message: 'El valor enviado como parámetro de búsqueda no es válido.' });
 
-    /**
-     * 
-     * Validar si el usuario es parnerth y verificar que tenga acceso a la lista
-     * 
-     */
-    // const user = await User.findById({ _id: req.user_id }, ['parnerth']);
-    // console.log(user);
+    let group;
 
     try {
 
-      const groupFound = await Group.findById(param, ['name', 'color', 'user_id', 'parnerth'])
-        .populate({ path: 'user_id', select: 'name img -_id' })
-        .populate({ path: 'parnerth', select: 'name img parnerth_key -_id' });
+      // Obtener el usuario en turno para validar si es USUARIO o PARNERTH
+      const userOnTurn = await User.findById(req.user_id, ['parnerth_key']);
 
-      if(!groupFound) throw { ok: false, error: 404, message: 'No se encontro el elemento.' };
+      // Validar si el Usuario en turno tiene un ['parnerth_key] o no
+      // Si es NUll es Usuario
+      // Si es EXISTE es Parnerth
+      if(userOnTurn.parnerth_key === null){
+        console.log('Usre');
+        const groupFound = await Group.findById( param );
+        if(!groupFound) throw { ok: false, error: 401, message: 'No se encontro el elemento.' };
+        
+        group = groupFound;
+        
+      }else{
 
+        // Obtener el producto y validar en el query si el ID del parnerth existe dentro de la propiedad pàra darle acceso a la información
+        const groupFound = await Group.findOne({ _id: param, parnerth: { $in: [req.user_id] } }, ['name', 'color', 'parnerth', 'createdAt'])
+          .populate({ path: 'parnerth', select: 'name img -_id' });
+
+        if(!groupFound) throw { ok: false, error: 401, message: 'No tienes acceso para ver esta información.' };
+
+        group = groupFound;
+        
+      };
+
+      // Obtener productos relacionados con el Grupo consultado
       const productsFound = await Product.find({ group: param }, [
-        'name',
-        'price',
-        'value',
-        'state',
-        'description',
-        'sold_date',
-        'createdAt'
+        '-group',
+        '-updatedAt',
+        '-user_id'
       ]).sort({ 'createdAt': -1 });
 
       const response = {
-        group: groupFound,
+        group,
         products: productsFound
       }
 
